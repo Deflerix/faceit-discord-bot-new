770696d7244827ae9e2110a9e31276856c37321a
Deflerix <111231693+Deflerix@users.noreply.github.com>
2026-04-05 15:44:34 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -151,16 +151,14 @@ client.once('ready', async () => {
   console.log(`Zalogowano jako ${client.user.tag}`);
   loadMatches();
 
+  // Fetch aplikacji zanim utworzymy komendy
+  await client.application.fetch();
+
   // Tworzenie komend Slash
   const commands = [
-    new SlashCommandBuilder()
-      .setName('checkmatch')
-      .setDescription('Sprawdza mecz')
-      .addStringOption(o => o.setName('nick').setRequired(true))
+    new SlashCommandBuilder().setName('checkmatch').setDescription('Sprawdza mecz').addStringOption(o => o.setName('nick').setRequired(true))
   ];
-
-  // Nadpisanie wszystkich komend w guildzie
-  await client.application.commands.set(commands, GUILD_ID);
+  for (const c of commands) await client.application.commands.create(c, GUILD_ID);
 
   // Cykliczne sprawdzanie meczy
   setInterval(() => { nicknames.forEach(n => processMatch(n)); }, Number(CHECK_INTERVAL) || 180000);
